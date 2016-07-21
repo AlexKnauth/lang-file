4dc0385056a7e723a788adfd42078d3766f4f5bf
AlexKnauth <alexander@knauth.org>
2016-07-21 10:14:46 -0400

    add read-lang-module

--- lang-file/read-lang-file.rkt
+++ lang-file/read-lang-file.rkt
@@ -2,7 +2,8 @@
 
 (require racket/contract/base)
 (provide (contract-out
-          [read-lang-file (-> path-string? syntax?)])
+          [read-lang-file (-> path-string? syntax?)]
+          [read-lang-module (-> input-port? syntax?)])
          lang-file?
          lang-file-lang
          )
@@ -19,16 +20,19 @@
 
 ;; read-lang-file : Path-String -> Syntax
 (define (read-lang-file path-string)
-  (define port (open-input-file path-string))
+  (call-with-input-file* path-string read-lang-module))
+
+;; read-lang-module : Input-Port -> Syntax
+(define (read-lang-module port)
   (port-count-lines! port)
-  (begin0
-    (with-module-reading-parameterization 
-     (lambda () 
-       (read-syntax (object-name port) port)))
-    (close-input-port port)))
+  (with-module-reading-parameterization 
+   (lambda () 
+     (read-syntax (object-name port) port))))
 
 ;; private value eq? to itself
 (define read-language-fail (gensym 'read-language-fail))
+(define (read-language-fail? v)
+  (eq? v read-language-fail))
 
 ;; lang-file? : Path-String -> Boolean
 (define (lang-file? path-string)
@@ -37,7 +41,7 @@
      (define port (open-input-file path-string))
      (port-count-lines! port)
      (begin0
-       (not (eq? (read-language port (λ () read-language-fail)) read-language-fail))
+       (not (read-language-fail? (read-language port (λ () read-language-fail))))
        (close-input-port port))]
     [else #false]))
 
@@ -45,20 +49,17 @@
 (define (lang-file-lang path-string)
   (cond
     [(file-exists? path-string)
-     (define port (open-input-file path-string))
-     (port-count-lines! port)
-     (begin0
-       (read-lang port)
-       (close-input-port port))]
+     (call-with-input-file* path-string read-lang)]
     [else #false]))
 
 ;; read-lang : Input-Port -> (U False String)
 (define (read-lang port)
+  (port-count-lines! port)
   (define port* (peeking-input-port port))
   (port-count-lines! port*)
   (and
    (with-handlers ([exn:fail:read? (λ (e) #false)])
-     (not (eq? (read-language port* (λ () read-language-fail)) read-language-fail)))
+     (not (read-language-fail? (read-language port* (λ () read-language-fail)))))
    (let* ([end (file-position port*)]
           [str (read-string end port)]
           [hash-lang-positions (regexp-match-positions* "#lang|#!" str)]
@@ -68,11 +69,42 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (module+ test
+  (define-check (check-stx-datum stx expected-datum)
+    (check-pred syntax? stx)
+    (check-equal? (syntax->datum stx) expected-datum))
+  
   (define-runtime-path read-lang-file.rkt "read-lang-file.rkt")
   (define-runtime-path scribblings "scribblings")
   (check-true (lang-file? read-lang-file.rkt))
   (check-false (lang-file? scribblings))
   (check-pred syntax? (read-lang-file read-lang-file.rkt))
+
+  (test-case "read-lang-module"
+    (define (read-mod . strs)
+      (call-with-input-string (apply string-append strs) read-lang-module))
+
+    (check-stx-datum (read-mod
+                      "#lang racket/base\n"
+                      "(+ 1 2)")
+                     '(module anonymous-module racket/base
+                        (#%module-begin (+ 1 2))))
+    (check-stx-datum (read-mod
+                      "#lang at-exp racket/base\n"
+                      "(+ 1 2)\n"
+                      "@+[3 4]\n"
+                      "@string-append{five five}\n"
+                      "(define six (number->string 6))\n"
+                      "(define seven (symbol->string 'seven))\n"
+                      "@string-append{The numbers @six and @|seven|.}")
+                     '(module anonymous-module racket/base
+                        (#%module-begin
+                         (+ 1 2)
+                         (+ 3 4)
+                         (string-append "five five")
+                         (define six (number->string 6))
+                         (define seven (symbol->string 'seven))
+                         (string-append "The numbers " six " and " seven "."))))
+    )
 
   (test-case "read-lang"
     (define (read-lang-from-strings . strs)
--- lang-file/scribblings/read-lang-file.scrbl
+++ lang-file/scribblings/read-lang-file.scrbl
@@ -17,6 +17,17 @@ source code: @url{https://github.com/AlexKnauth/lang-file}
 Reads a @hash-lang[] file into a @racket[module] syntax object.
 }
 
+@defproc[(read-lang-module [port input-port?]) syntax?]{
+Reads the contents of a @hash-lang[] file from @racket[port] into a
+@racket[module] syntax object.
+
+@examples[
+  (require lang-file/read-lang-file)
+  (read-lang-module (open-input-string "#lang racket/base\n(+ 1 2)"))
+  (syntax->datum
+   (read-lang-module (open-input-string "#lang racket/base\n(+ 1 2)")))
+]}
+
 @defproc[(lang-file? [path-string path-string?]) boolean?]{
 Returns true if @racket[path-string] points to a valid @hash-lang[]
 file, false otherwise.
